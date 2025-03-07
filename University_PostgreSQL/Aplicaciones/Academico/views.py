f25999de9b52b94930a58b4095e4cade48798bd4
mayrars <mayra203@gmail.com>
2025-03-07 07:01:52 -0600

    created view and route

--- University_PostgreSQL/Aplicaciones/Academico/views.py
+++ University_PostgreSQL/Aplicaciones/Academico/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Bienvenido a la aplicación Académica")
+    cursosListados = Curso.objects.all()
+    return render(request, 'gestionCursos.html', {'cursos': cursosListados})
