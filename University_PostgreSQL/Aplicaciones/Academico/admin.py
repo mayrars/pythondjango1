59b22932ac2c37e0319475d8941ff03b5036bf46
mayrars <mayra203@gmail.com>
2025-03-11 07:03:50 -0600

    changed models and views

--- University_PostgreSQL/Aplicaciones/Academico/admin.py
+++ University_PostgreSQL/Aplicaciones/Academico/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import Curso
 
 # Register your models here.
-admin.site.register(Curso)
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'creditos')
+    #ordering = ('creditos','nombre',)
+    #search_fields = ('nombre', 'creditos')
+    #list_editable = ('nombre',)
+    #list_display_links = ('nombre',)
+    #list_filter = ('creditos',)
+    #list_per_page = 3
+    exclude = ('creditos',)
+
+#admin.site.register(Curso)
+#admin.site.register(Curso, CursoAdmin)
--- University_PostgreSQL/Aplicaciones/Academico/models.py
+++ University_PostgreSQL/Aplicaciones/Academico/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     creditos = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto="{0} ({1})".format(self.nombre, self.creditos)
+        return texto
