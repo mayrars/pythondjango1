e9164d1c0586f97f5e01a70e47aec9d96ddd120c
mayrars <mayra203@gmail.com>
2025-03-12 05:45:18 -0600

    changed title and return data

--- University_PostgreSQL/Aplicaciones/Academico/views.py
+++ University_PostgreSQL/Aplicaciones/Academico/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 from .models import Curso
 
 # Create your views here.
 def home(request):
-    #cursosListados = Curso.objects.all()
+    cursosListados = Curso.objects.all()
     #cursosListados = Curso.objects.all()[:5]
     #cursosListados = Curso.objects.all()[4:9]
     #cursosListados = Curso.objects.all().order_by('nombre')
@@ -12,5 +13,23 @@ def home(request):
     #cursosListados = Curso.objects.filter(nombre='Historia',creditos=4)
     #cursosListados = Curso.objects.filter(creditos__lte=4)
     #cursosListados = Curso.objects.filter(nombre__startswith='Q')
-    cursosListados = Curso.objects.filter(nombre__contains='g')
-    return render(request, 'gestionCursos.html', {'cursos': cursosListados})
+    #cursosListados = Curso.objects.filter(nombre__contains='g')
+
+    data = {
+        'titulo': 'Gestion de Cursos',
+        'cursos': cursosListados
+    }
+    #return render(request, 'gestionCursos.html', {'cursos': cursosListados})
+    return render(request, 'gestionCursos.html', data)
+
+class CursoListView(ListView):
+    model = Curso
+    template_name = 'gestionCursos.html'
+
+    def get_queryset(self):
+        return Curso.objects.filter(creditos__lte=4)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Gestion de Cursos'
+        return context
